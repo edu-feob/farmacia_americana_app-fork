7908edf584ee962a7c1fcf8ff23a4aaa882ff78b
Henrique Molinari <henrique.molinari@sou.unifeob.edu.br>
2026-03-17 19:57:15 -0300

    Commit - Tela de Login e pasta widgets

---
+++ farmacinha_app/lib/widgets/login_field.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import '../pallete.dart';
+
+class LoginField extends StatelessWidget {
+  final String hintText;
+  const LoginField({super.key, required this.hintText});
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: const BoxConstraints(maxWidth: 330),
+      child: TextFormField(
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.all(20),
+          enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              color: Pallete.borderColor,
+              width: 3,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              color: Pallete.actionButton,
+              width: 3,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          hintText: hintText,
+        ),
+      ),
+    );
+  }
+}
