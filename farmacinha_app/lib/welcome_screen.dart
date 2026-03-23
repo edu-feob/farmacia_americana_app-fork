6a8b26b53019f8ddc0b77d9ff2f1ce2c909312d1
Henrique Molinari <henrique.molinari@sou.unifeob.edu.br>
2026-03-22 22:11:12 -0300

    feat(welcome): adiciona logo da empresa

--- farmacinha_app/lib/welcome_screen.dart
+++ farmacinha_app/lib/welcome_screen.dart
@@ -6,10 +6,29 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Pallete.backgroundColor,
       body: SafeArea(
-        child: SizedBox.expand(),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const SizedBox(height: 60),
+
+                Image.asset(
+                  'assets/images/logo.png',
+                  width: 260,
+                  fit: BoxFit.contain,
+                ),
+
+                const SizedBox(), // placeholder
+                const SizedBox(), // placeholder
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
