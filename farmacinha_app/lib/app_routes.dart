fbe30de1228b2e5256b1a867630bc2b318b66e52
Henrique Molinari <henrique.molinari@sou.unifeob.edu.br>
2026-03-23 21:37:44 -0300

    feat(navigation): adiciona mapa de rotas no AppRoutes

--- farmacinha_app/lib/app_routes.dart
+++ farmacinha_app/lib/app_routes.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:farmacia_app/splash_screen.dart';
+import 'package:farmacia_app/welcome_screen.dart';
+import 'package:farmacia_app/login_screen.dart';
+import 'package:farmacia_app/register_screen.dart';
 
 class AppRoutes {
   static const String splash = '/';
   static const String welcome = '/welcome';
   static const String login = '/login';
   static const String register = '/register';
+
+  static Map<String, WidgetBuilder> get routes => {
+        splash: (_) => const SplashScreen(),
+        welcome: (_) => const WelcomeScreen(),
+        login: (_) => const LoginScreen(),
+        register: (_) => const RegisterScreen(),
+      };
 }
